4efb8ec3d4ed4c26a3a4d5e72a2b3de5fbb13c86
JacksonRCode <jackdareid@gmail.com>
2026-02-11 09:06:02 -0500

    Finished a confusing join query... but now I know how to do it!
    In retrievalQueries.js, I finished the query that returns a user's
    previous workout stats to them.

--- backend/db/queries/retrievalQueries.js
+++ backend/db/queries/retrievalQueries.js
@@ -2,6 +2,7 @@
 
 const pool = require("./poolConnection.js");
 
+// 1. Retrieve user data
 const getUserData = async (email) => {
   /*
    * This function retrieves user data, using their email to query the database.
@@ -24,6 +25,7 @@ const getUserData = async (email) => {
   }
 };
 
+// 1.1. Check if email is in DB
 const checkEmail = async (email) => {
   /*
    * This function checks to see if an email is already being used in the database.
@@ -36,7 +38,7 @@ const checkEmail = async (email) => {
   const queryText = `SELECT 1 FROM users WHERE email = $1;`;
 
   try {
-    const res = pool.query(queryText, [email]);
+    const res = await pool.query(queryText, [email]);
     return res;
   } catch (err) {
     console.error("Error accessing database.");
@@ -45,15 +47,121 @@ const checkEmail = async (email) => {
   }
 };
 
-// 1.1. Check if email already exists (for sign-up)
-
 // 2. Get user programs
+const getPrograms = async (user_id) => {
+  /*
+   * This function retrieves a users programs using their user_id.
+   *
+   * Tested: True
+   *
+   * Accepts: user_id
+   * Returns: List of program objects
+   */
+  const queryText = `SELECT * FROM programs WHERE user_id = $1;`;
+
+  try {
+    const res = await pool.query(queryText, [user_id]);
+    console.log(`Result: ${res}`);
+    return res;
+  } catch (err) {
+    console.error(`Error retrieving user programs.`);
+    throw err;
+  }
+};
 
 // 3. Get user workouts
+const getUserWorkouts = async (user_id) => {
+  /*
+   * This function retrieves a users workouts using their user_id.
+   *
+   * Tested: True
+   *
+   * Accepts: user_id
+   * Returns: List of workout objects
+   */
+
+  const queryText = `SELECT * FROM workouts WHERE user_id = $1;`;
+
+  try {
+    const res = await pool.query(queryText, [user_id]);
+    return res;
+  } catch (err) {
+    console.error("Error retrieving users workouts.");
+    throw err;
+  }
+};
 
 // 4. Get program workouts
+const getProgramWorkouts = async (program_id) => {
+  /*
+   * This function returns all workouts associated with a program.
+   *
+   * Tested: True
+   *
+   * Accepts: program_id.
+   * Returns: List of workouts.
+   */
+
+  const queryText = `SELECT * FROM workouts WHERE program_id = $1;`;
+
+  try {
+    const res = await pool.query(queryText, [program_id]);
+    return res;
+  } catch (err) {
+    console.error("Error retrieving program workouts.");
+    throw err;
+  }
+};
 
 // 5. Get stats from previous iteration of current workout
+const getPreviousWorkout = async (workout_id) => {
+  /*
+   * This function finds all exercises associated with the provided workout and returns the users
+   * stats in order to help them remember what they should be lifting this time.
+   *
+   * Tested: False
+   *
+   * Accepts: workout_id
+   * Returns List of weight values: one for each exercise.
+   */
+  // (workout_id) --> workout_exercises (for order_index of each exercise)
+  // Get list of exercises
+
+  const completed_workout_query = `SELECT * FROM workout_completed WHERE workout_id = $1;`;
+  let completed_workouts;
+  try {
+    completed_workouts = await pool.query(completed_workout_query, [
+      workout_id,
+    ]);
+  } catch (err) {
+    console.error("FAIL!");
+    throw err;
+  }
+
+  // JOIN on completed sets to get the specifics of each completed exercise
+  // const join_query = `SELECT * FROM completed_exercises OUTER JOIN completed_sets ON completed_exercises.completed_exercise_id=completed_sets.completed_exercise_id;`;
+  const join_query_2 = `SELECT 
+    ce.completed_exercise_id, ce.exercise_id, ce.workout_completed_id, ce.notes,
+    cs.* 
+    FROM completed_exercises ce
+    LEFT JOIN completed_sets cs 
+    ON ce.completed_exercise_id = cs.completed_exercise_id
+    WHERE ce.workout_completed_id = $1
+    ORDER BY 
+      ce.completed_exercise_id ASC,
+      cs.set_number;`;
+  let workout_details;
+  try {
+    workout_details = await pool.query(join_query_2, [
+      completed_workouts.rows[0].workout_completed_id,
+    ]);
+  } catch (err) {
+    console.error("Failed to join");
+    throw err;
+  }
+
+  return workout_details;
+};
 
 // 6.  all exercises
 
@@ -61,9 +169,18 @@ const checkEmail = async (email) => {
 
 // 8. Authenticate email & password
 
+// (async () => {
+//   const value1 = await checkEmail("testy@gmail.com");
+//   console.log(`Good email: ${value1.rowCount}`);
+//   const value2 = await checkEmail("testfail@gmail.com");
+//   console.log(`Fail email: ${value2.rowCount}`);
+// })();
+
+// (async () => {
+//   const res = await getProgramWorkouts(1);
+//   console.log(res.rows[0]);
+// })();
+
 (async () => {
-  const value1 = await checkEmail("testy@gmail.com");
-  console.log(`Good email: ${value1.rowCount}`);
-  const value2 = await checkEmail("testfail@gmail.com");
-  console.log(`Fail email: ${value2.rowCount}`);
+  await getPreviousWorkout((workout_id = 3));
 })();
---
+++ backend/db/seedWorkout.js
@@ -0,0 +1,73 @@
+const {
+  createWorkout,
+  createWorkoutExercises,
+  createCompletedWorkout,
+  createCompletedExercise,
+  createCompletedSet,
+} = require("./queries/inputQueries.js");
+
+// (async () => {
+//   const res_1 = await createWorkout(
+//     1,
+//     1,
+//     "Practical",
+//     "This is a practical test",
+//   );
+//   console.log(res_1);
+// })();
+
+// async () => {
+//   const res = await createWorkoutExercises(
+//     1,
+//     3,
+//     1,
+//     2,
+//     10,
+//     90,
+//     "FALSE",
+//     0,
+//     "No notes",
+//   );
+//
+//   const res_2 = await createWorkoutExercises(
+//     2,
+//     3,
+//     2,
+//     2,
+//     10,
+//     90,
+//     "FALSE",
+//     0,
+//     "No notes",
+//   );
+//   const res_3 = await createWorkoutExercises(
+//     (exercise_id = 3),
+//     (workout_id = 3),
+//     (order_index = 3),
+//     (sets = 3),
+//     (reps = 3),
+//     (rest = 90),
+//     (time_f = "FALSE"),
+//     (distance = 0),
+//     (notes = "ANYTHING"),
+//   );
+// };
+
+// (async () => {
+//   const res = createCompletedWorkout(1, 3, "No notes!!");
+// })();
+
+// (async () => {
+//   const res_1 = createCompletedExercise(1, 1, 2, "FALSE", "Is santa real?");
+//   const res_3 = createCompletedExercise(1, 2, 2, "FALSE", "PR");
+//   const res_5 = createCompletedExercise(1, 3, 2, "FALSE", "Itchy bum");
+// })();
+
+(async () => {
+  const set_1 = await createCompletedSet(2, 100, 10, 6, 1);
+  const set_2 = await createCompletedSet(2, 135, 8, 7, 2);
+  const set_3 = await createCompletedSet(3, 90, 10, 1, 1);
+  const set_4 = await createCompletedSet(3, 200, 10, 2, 2);
+  const set_5 = await createCompletedSet(4, 300, 10, 3, 1);
+  const set_6 = await createCompletedSet(4, 400, 10, 7, 2);
+})();
