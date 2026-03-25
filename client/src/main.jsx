bfc9d12ebc7af4b8a16d830414b78b0f4f8196d4
JacksonRCode <jackdareid@gmail.com>
2026-03-25 15:52:03 -0400

    Start implementing react-router into project.
    Need more url and componenet separation so I am going to use the react
    router. Don't know how to use it but we are trying her out.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router';
+import App from './App.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/api" element={<App />} />
+        <Route path="/api/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
